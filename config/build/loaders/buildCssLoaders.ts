b86af1f7a881c83bd116b344dc80f85dcf3f9bab
marica <2cadmaster@gmail.com>
2023-04-07 15:25:29 +0300

    fix prod

--- config/build/loaders/buildCssLoaders.ts
+++ config/build/loaders/buildCssLoaders.ts
@@ -1,12 +1,15 @@
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
 export function buildCssLoaders(isDev: boolean){
+    console.log('!!!!isDev', isDev)
     return  {
         test: /\.s[ac]ss$/i,
         use: [
             // Creates `style` nodes from JS strings
+            
+            
             isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-          
+            // MiniCssExtractPlugin.loader,
             // Translates CSS into CommonJS
             {
                 loader: "css-loader",
@@ -16,7 +19,7 @@ export function buildCssLoaders(isDev: boolean){
                             path.includes('.module.')),
                         localIdentName: isDev 
                             ? '[path][name]__[local]' 
-                            : '[hash: base64: 8]'
+                            : '[hash:base64:8]'
                     },
                 }
             },
