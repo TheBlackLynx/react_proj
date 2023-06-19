85eca0aa3a4ce54de3624b66998dd53a17eefe4d
marica <2cadmaster@gmail.com>
2023-06-19 08:26:01 +0300

    chore: add drawer to notification button component

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -1,11 +1,13 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import cls from './NotificationButton.module.scss';
-import { memo } from 'react';
+import { memo, useCallback, useState } from 'react';
 import { Popover } from 'shared/ui/Popups';
 import { AppButton, AppButtonTheme, HStack, Icon } from 'shared';
 import { NotificationList } from 'entities/Notification';
 import Notification from 'shared/assets/icons/Notification.svg'
+import { Drawer } from 'shared/ui/Drawer/Drawer';
+import { BrowserView, MobileView } from 'react-device-detect';
 
 interface NotificationButtonProps {
     className?: string;
@@ -14,21 +16,37 @@ interface NotificationButtonProps {
 export const NotificationButton = memo((props: NotificationButtonProps) => {
     const { className } = props;
     const { t } = useTranslation();
+    const [isOpen, isOpenHandler ]  = useState(false);
+
+    const onOpenDrawer = useCallback(() => {
+        isOpenHandler(true)
+    }, [isOpenHandler])
+
+    const onCloseDrawer = useCallback(() => {
+        isOpenHandler(false)
+    }, [isOpenHandler])
     
+    const trigger = (
+        <AppButton buttonTheme={AppButtonTheme.CLEAR} onClick={onOpenDrawer}>
+            <Icon Svg={Notification} inverted/>
+        </AppButton>
+    )
     return (
         <div className={classNames(cls.NotificationButton, {}, [className])}>
-            <HStack gap={"16"}
-                    
-            >
-                <Popover trigger={(
-                    <AppButton buttonTheme={AppButtonTheme.CLEAR}>
-                        <Icon Svg={Notification} inverted/>
-                    </AppButton>
-                )}
-                    className={cls.actions}
-                >
+            <BrowserView>
+                <HStack gap={"16"}>
+                    <Popover trigger={trigger} className={cls.actions}>
+                        <NotificationList className={cls.notifications}/>
+                    </Popover> </HStack>
+            </BrowserView>
+            <MobileView>
+                {trigger}
+                <Drawer isOpen={isOpen} onClose={onCloseDrawer} className={null}>
                     <NotificationList className={cls.notifications}/>
-                </Popover> </HStack>
+                </Drawer>
+            </MobileView>
+           
+            
         </div>
     );
 });
